5626c33684f8dac8f776e8b513dbbc9146c41467
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-26 11:04:37 -0800

    Self Practice

---
+++ alx-low_level_programming/0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - to print both lower case and upper case
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	for ( i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for ( i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+}
