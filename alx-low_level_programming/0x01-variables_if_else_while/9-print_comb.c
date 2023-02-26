85c67af4304d02dcbffbba008dde239a7b587925
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-26 13:53:30 -0800

    Self Practice

--- alx-low_level_programming/0x01-variables_if_else_while/9-print_comb.c
+++ alx-low_level_programming/0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,18 @@
 
 int main(void)
 {
-	int i
+	int i;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		if (i == 9)
 			putchar(i +'0');
-		putchar(i +'0');
-		putchar(',');
-		putchar(' ');
+		else
+		{
+			putchar(i +'0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
