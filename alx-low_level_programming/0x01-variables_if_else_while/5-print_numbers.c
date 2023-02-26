b948851d6bdcaa9b7c31be186cdff9d9b0f3a544
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-26 12:12:28 -0800

    Self Practice

---
+++ alx-low_level_programming/0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print all single numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
