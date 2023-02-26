44bff2822956298a53e5bfa05123eba997fc0260
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-26 09:26:51 -0800

    Self Practice

---
+++ alx-low_level_programming/0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print all the alphabets
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	for(char j='a' ; j<= 'z' ; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+}
