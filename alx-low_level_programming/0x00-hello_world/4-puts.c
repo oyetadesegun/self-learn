2d372fa8cc98d11a3e0a93ec272b01c5a0360325
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-26 04:18:37 -0800

    Self Practice

---
+++ alx-low_level_programming/0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Print a line using puts
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
