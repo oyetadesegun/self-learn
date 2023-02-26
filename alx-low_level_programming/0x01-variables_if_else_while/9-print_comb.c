5d304600666154f6eedcd7d3ffc2241df42c0a19
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-26 13:48:06 -0800

    Self Practice

---
+++ alx-low_level_programming/0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of single-digit numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		if (i == 9)
+			putchar(i +'0');
+		putchar(i +'0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
