a7cde0837dcb6b76938db8dbb0999746a1b3a058
Oyetade Segun <johnwellacademy@gmail.com>
2023-02-27 04:21:04 -0800

    Self Practice

---
+++ alx-low_level_programming/0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Print the twos of numbers followed by a comma
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(j + '0');
+			putchar('\n');
+			for (k = 0; k = 9; k++)
+			{
+				putchar(k + '0');
+				for (l = 0; l = 9; l++)
+				{
+					putchar(l + '0');
+					if(i + j + k + l != 35)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
