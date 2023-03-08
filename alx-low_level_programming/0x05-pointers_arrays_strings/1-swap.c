93bb09583103b4977f08b4012c90e1791d14fef3
Oyetade Segun <johnwellacademy@gmail.com>
2023-03-08 12:59:41 -0800

    Added

---
+++ alx-low_level_programming/0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - a function that swap the value of two integers
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, *int b)
+{
+	int m;
+	m = *a;
+	*a = *b;
+	*b = m;
+}
+
