8c8309c1b39816e85b8c3ba8aef67076be248218
oyetadesegun <johnwellacademy@gmail.com>
2023-03-10 17:36:45 +0100

    r

---
+++ alx-low_level_programming/0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - A function that concatenates two strings
+ * @dest: the destination
+ * @src: source
+ * Return: *dest
+*/
+char *_strcat(char *dest, char *src)
+{
+    int len = 0, i;
+    
+    while (dest[len])
+        len++;
+    for (i = 0 ; src[i] != '\0'; i++)
+    {
+        dest[len] = src[i];
+        len += 1;
+    }
+    dest[len] = '\0';
+}
